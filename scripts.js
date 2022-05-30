d227d82469e2dc4c3c0fc66f867b7a8e6196a643
Runar <runarnazmutdinov@gmail.com>
2022-05-30 16:41:17 +0300

    rename fn

--- scripts.js
+++ scripts.js
@@ -1,5 +1,4 @@
 const loadBtn = document.querySelector(".js-load");
-const resultsContainer = document.querySelector(".js-results");
 const searchInput = document.querySelector(".js-input");
 const cardContainer = document.querySelector(".card-container");
 const responseContainer = document.querySelector(".response-container");
@@ -9,6 +8,7 @@ const jsonPlApiUrl = "https://jsonplaceholder.typicode.com/posts";
 
 function createMarkup(data) {
   if (!data.length) {
+    console.log(data);
     responseContainer.innerHTML += `
         <img src="${data.avatar_url}">
         <p> Имя: <span>${data.name}</span><p>
@@ -18,7 +18,8 @@ function createMarkup(data) {
   }
 
   if (data.length) {
-    data?.map((user) => {
+    console.log(data);
+    data.map((user) => {
       cardContainer.innerHTML += `
         <div class="user-card">
           <img src=${"https://i.pravatar.cc/300"}>
@@ -37,7 +38,7 @@ loadBtn.addEventListener("click", function (evt) {
   evt.preventDefault();
   const searchValue = searchInput.value.trim().toLowerCase();
 
-  function errorMessage() {
+  function notifyUser() {
     return (responseContainer.innerHTML = `
             <h1 class="error">Пользователь не найден</h1>
         `);
@@ -46,7 +47,7 @@ loadBtn.addEventListener("click", function (evt) {
   if (searchValue !== "") {
     fetch(`${gitHubApiUrl}${searchValue}`)
       .then((response) =>
-        response.ok ? response.json() : new Promise(errorMessage)
+        response.ok ? response.json() : new Promise(notifyUser)
       )
       .then((data) => createMarkup(data))
       .catch((error) => console.log(error));
